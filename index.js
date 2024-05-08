71e40f408e9a6acfd587e1f53a65b916839a8a7b
IsaacMasisV <isaacmv2003@gmail.com>
2024-05-08 15:49:55 -0600

    commit del miercoles

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+document.getElementById("btn_IniciarSesion").addEventListener("click",IniciarSesion);
+
+document.getElementById("btn_Registrarse").addEventListener("click",register);
+
+
 //variables//
+
 var Contenedor_Login_Register = document.querySelector(".Contenedor_Login_Register");
 
 var formulario__login = document.querySelector(".formulario__login");
@@ -10,13 +16,19 @@ var caja__traseraLogin = document.querySelector(".caja__traseraLogin");
 var caja__traseraRegistrar = document.querySelector(".caja__traseraRegistrar");
 
 
+
+function IniciarSesion() {
+    formulario__Registro.style.display  = "none";
+    Contenedor_Login_Register.style.left = "-200px";
+    formulario__login.style.display = "block";
+    caja__traseraRegistrar.style.opacity ="1";
+    caja__traseraLogin.style.opacity = "0";
+}
+
 function register() {
     formulario__Registro.style.display  = "block";
     Contenedor_Login_Register.style.left = "200px";
     formulario__login.style.display = "none";
     caja__traseraRegistrar.style.opacity ="0";
     caja__traseraLogin.style.opacity = "1";
-    
-
-
 }
