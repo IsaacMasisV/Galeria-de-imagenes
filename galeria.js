f2dd5ea485629d32ff568a4d66e5fa0ffefe3c16
IsaacMasisV <isaacmv2003@gmail.com>
2024-05-09 16:05:58 -0600

    commit jueves

---
+++ galeria.js
@@ -0,0 +1,11 @@
+const fullimgbox = document.getElementById("fullimgbox"),
+fullimg = document.getElementById("fullimg");
+
+function openfullimg(refarance){
+    fullimgbox.style.display = "flex"
+    fullimg.src = refarance
+}
+
+function closeImg(){
+    fullimgbox.style.display = "none";
+}
